9e61241521eed5d4503415e738656977a39cc609
always2024 <14181317+always2024@user.noreply.gitee.com>
2025-02-20 19:52:54 +0800

    提交

---
+++ log/log.go
@@ -0,0 +1,48 @@
+package log
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+// 日志实例
+var (
+	errorLog = log.New(os.Stdout, "\033[31m[error]\033[0m ", log.LstdFlags|log.Lshortfile)
+	infoLog  = log.New(os.Stdout, "\033[34m[info ]\033[0m ", log.LstdFlags|log.Lshortfile)
+	loggers  = []*log.Logger{errorLog, infoLog}
+	mu       sync.Mutex
+)
+
+// 日志实例
+var (
+	Error  = errorLog.Println
+	Errorf = errorLog.Printf
+	Info   = infoLog.Println
+	Infof  = infoLog.Printf
+)
+
+// 三个日志层级
+const (
+	InfoLevel = iota //有点6
+	ErrorLevel
+	Disabled
+)
+
+// SetLevel controls log level
+func SetLevel(level int) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for _, logger := range loggers {
+		logger.SetOutput(os.Stdout)
+	}
+
+	if ErrorLevel < level {
+		errorLog.SetOutput(ioutil.Discard)
+	}
+	if InfoLevel < level {
+		infoLog.SetOutput(ioutil.Discard)
+	}
+}
---
+++ principle_explanation/engine.md
@@ -0,0 +1,6 @@
+# 对于Engine这个概念的解释
+
+## Engine是什么
+
+
+## 如何实现Engine
